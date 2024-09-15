c3f78d1bc19c4d48b67a9161bcf5e8b7d6569263
Pavan Varma <pavan@scrupulouslabs.com>
2024-09-15 19:32:53 +0530

    Initial Commit

---
+++ eachday/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  eachday
+//
+//  Created by pavan varma on 15/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ eachday/eachdayApp.swift
@@ -0,0 +1,17 @@
+//
+//  eachdayApp.swift
+//  eachday
+//
+//  Created by pavan varma on 15/09/24.
+//
+
+import SwiftUI
+
+@main
+struct eachdayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
